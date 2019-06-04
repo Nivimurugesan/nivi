63e80a40b97d467e7e7ef101102025032a33c2ef
Nivimurugesan <51353330+Nivimurugesan@users.noreply.github.com>
2019-06-04 10:14:48 +0530

    Update nivi1.c

--- nivi1.c
+++ nivi1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    char c;
+    char s;
     int isLowercaseVowel, isUppercaseVowel;
     printf("Enter an alphabet: ");
-    scanf("%c",&c);
-    isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+    scanf("%c",&s);
+    isLowercaseVowel = (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u');
+    isUppercaseVowel = (s == 'A' || s == 'E' || s == 'I' || s == 'O' || s == 'U');
     if (isLowercaseVowel || isUppercaseVowel)
-        printf("%c is a vowel.", c);
+        printf("%c is a vowel.", s);
     else
-        printf("%c is a consonant.", c);
+        printf("%c is a consonant.", s);
     return 0;
 }
